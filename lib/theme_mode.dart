7d16e4ffb90e3d080962cd51de7f610381e36f52
Santos <hanaheena93@gmail.com>
2023-05-28 16:42:12 +0300

    Theme Change

--- lib/theme_mode.dart
+++ lib/theme_mode.dart
@@ -1,25 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ThemeStyle {
   static const Color lightColor = Color(0xFFB7935F);
+  static const Color darkColor = Color(0xFF141A2E);
+  static const Color yellowColor = Color(0xFFFACC1D);
   static ThemeData lightTheme = ThemeData(
-      primaryColor: Color(0xFFB7935F),
+      colorScheme: ColorScheme(
+          brightness: Brightness.light,
+          primary: lightColor,
+          onPrimary: Colors.black54,
+          secondary: Colors.black,
+          onSecondary: Colors.white,
+          error: Colors.red,
+          onError: Colors.white,
+          background: Colors.transparent,
+          onBackground: lightColor,
+          surface: Colors.grey.shade300,
+          onSurface: Colors.black54),
+      scaffoldBackgroundColor: Colors.transparent,
+      primaryColor: lightColor,
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        type: BottomNavigationBarType.fixed,
         backgroundColor: lightColor,
+        type: BottomNavigationBarType.fixed,
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.white,
       ),
-      scaffoldBackgroundColor: Colors.transparent,
       textTheme: TextTheme(
-          bodyLarge: TextStyle(
-              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
-          bodyMedium: TextStyle(
-              fontSize: 25, fontWeight: FontWeight.bold, color: lightColor),
-          bodySmall: TextStyle(
-              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+        bodySmall: GoogleFonts.elMessiri(
+            fontSize: 20, fontWeight: FontWeight.w600, color: lightColor),
+        bodyMedium: GoogleFonts.elMessiri(
+            fontSize: 25, fontWeight: FontWeight.bold, color: Colors.black54),
+        bodyLarge: GoogleFonts.elMessiri(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+      ),
       appBarTheme: AppBarTheme(
-        color: Colors.transparent,
-        elevation: 0.0,
-      ));
+          iconTheme: IconThemeData(color: lightColor, size: 35),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          centerTitle: true));
+  static ThemeData darkTheme = ThemeData(
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: Colors.transparent,
+      primaryColor: darkColor,
+      colorScheme: ColorScheme(
+          brightness: Brightness.dark,
+          primary: darkColor,
+          onPrimary: Colors.white,
+          secondary: yellowColor,
+          onSecondary: Colors.white,
+          error: Colors.red,
+          onError: Colors.white,
+          background: Colors.transparent,
+          onBackground: darkColor,
+          surface: Colors.grey.shade300,
+          onSurface: Colors.white),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: darkColor,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: yellowColor,
+        unselectedItemColor: Colors.white,
+      ),
+      textTheme: TextTheme(
+        bodySmall: GoogleFonts.elMessiri(
+            fontSize: 20, fontWeight: FontWeight.w600, color: yellowColor),
+        bodyMedium: GoogleFonts.elMessiri(
+            fontSize: 25, fontWeight: FontWeight.bold, color: Colors.white),
+        bodyLarge: GoogleFonts.elMessiri(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+      ),
+      appBarTheme: AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.white, size: 35),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          centerTitle: true));
 }
