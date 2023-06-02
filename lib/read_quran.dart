89c8f1aafb4cad8dba96b3d7e85e93b4d78131b8
Santos <hanaheena93@gmail.com>
2023-06-02 16:40:54 +0300

    Change Box view Project

--- lib/read_quran.dart
+++ lib/read_quran.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:islami/SuraArgs.dart';
+import 'package:islami/theme_mode.dart';
 
 class ReadQuran extends StatefulWidget {
   static const String routeName = 'readQuran';
@@ -27,28 +28,36 @@ class _ReadQuranState extends State<ReadQuran> {
               fit: BoxFit.fill,
             )),
         Scaffold(
-            appBar: AppBar(
-                title: Text(
-              suraArgs.NameSura,
-              style: Theme.of(context).textTheme.bodyLarge,
-            )),
-            body: verses.isEmpty
-                ? Center(child: CircularProgressIndicator())
-                : ListView.builder(
+          appBar: AppBar(
+              title: Text(
+            suraArgs.NameSura,
+            style: Theme.of(context).textTheme.bodyLarge,
+          )),
+          body: verses.isEmpty
+              ? Center(child: CircularProgressIndicator())
+              : Card(
+                  elevation: 12,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                      side: BorderSide(color: ThemeStyle.lightColor)),
+                  margin: EdgeInsets.symmetric(vertical: 28, horizontal: 16),
+                  child: ListView.builder(
                     itemBuilder: (context, index) {
                       return Text(verses[index]);
                     },
                     itemCount: verses.length,
-                  )),
+                  ),
+                ),
+        ),
       ],
     );
   }
 
   Future<void> loadQuran(int index) async {
-    String sura = await rootBundle.loadString("assets/files/$index");
+    String sura = await rootBundle.loadString("assets/files/${index + 1}.txt");
 
     List<String> lines = sura.split("\n");
-
+    print(lines);
     verses = lines;
 
     setState(() {});
