c62defb4d5b0c237f5cf997de5e95d1027ad0867
Santos <hanaheena93@gmail.com>
2023-06-02 15:45:46 +0300

    File Read Quran Changes

--- lib/read_quran.dart
+++ lib/read_quran.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islami/SuraArgs.dart';
 
-class ReadQuran extends StatelessWidget {
+class ReadQuran extends StatefulWidget {
   static const String routeName = 'readQuran';
+
+  @override
+  State<ReadQuran> createState() => _ReadQuranState();
+}
+
+class _ReadQuranState extends State<ReadQuran> {
+  List<String> verses = [];
 
   @override
   Widget build(BuildContext context) {
     SuraArgs suraArgs = ModalRoute.of(context)?.settings.arguments as SuraArgs;
-
+    if (verses.isEmpty) {
+      loadQuran(suraArgs.index);
+    }
     return Stack(
       children: [
         SizedBox(
@@ -17,13 +27,30 @@ class ReadQuran extends StatelessWidget {
               fit: BoxFit.fill,
             )),
         Scaffold(
-          appBar: AppBar(
-              title: Text(
-            suraArgs.NameSura,
-            style: Theme.of(context).textTheme.bodyLarge,
-          )),
-        ),
+            appBar: AppBar(
+                title: Text(
+              suraArgs.NameSura,
+              style: Theme.of(context).textTheme.bodyLarge,
+            )),
+            body: verses.isEmpty
+                ? Center(child: CircularProgressIndicator())
+                : ListView.builder(
+                    itemBuilder: (context, index) {
+                      return Text(verses[index]);
+                    },
+                    itemCount: verses.length,
+                  )),
       ],
     );
+  }
+
+  Future<void> loadQuran(int index) async {
+    String sura = await rootBundle.loadString("assets/files/$index");
+
+    List<String> lines = sura.split("\n");
+
+    verses = lines;
+
+    setState(() {});
   }
 }
